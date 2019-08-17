ae6c6070a4646ffd044fd9374dff54cac0d5ce63
Ryan Krol <ryankr@hotmail.co.uk>
2019-08-17 20:11:08 +0100

    Add code to access "sensitive" data

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM swift
 WORKDIR /app
 COPY . ./
-CMD swift package clean
-CMD swift run
-
+RUN apt-get update
+RUN echo "y" | apt-get install openssl libssl-dev
+RUN git pull
+RUN swift package clean
+RUN swift package update
+RUN swift build -c release
+RUN mv `swift build -c release --show-bin-path` /app/bin
+CMD /app/bin/GitEncryptionTest
--- Sources/GitEncryptionTest/main.swift
+++ Sources/GitEncryptionTest/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Foundation
+
+import SwiftToolbox
+
+do {
+    let config: EmailConfig = try ConfigHandler<EmailConfig>(configFile: "./../Config.json", relativeFrom: #file).load()
+    print("Hello, world!")
+    print("Have some sensitive data!")
+    print(config)
+} catch {
+    print("This did not work")
+    print(error)
+}
