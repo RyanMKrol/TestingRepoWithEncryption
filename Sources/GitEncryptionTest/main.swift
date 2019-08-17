5248579cd7c7eaf7bf2a401bee2ae5601ce5e8dc
Ryan Krol <ryankr@hotmail.co.uk>
2019-08-17 21:12:31 +0100

    Updating encrypted config files

--- Sources/GitEncryptionTest/main.swift
+++ Sources/GitEncryptionTest/main.swift
@@ -4,7 +4,6 @@ import SwiftToolbox
 
 do {
     let config: EmailConfig = try ConfigHandler<EmailConfig>(configFile: "./../Config.json", relativeFrom: #file).load()
-    let Otherconfig: EmailConfig = try ConfigHandler<EmailConfig>(configFile: "./../OtherConfig.json", relativeFrom: #file).load()
     print("Success")
 } catch {
     print("This did not work")
