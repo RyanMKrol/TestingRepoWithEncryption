adcc0841cc352905c71a48a4c6f41d499d646e37
Ryan Krol <ryankr@hotmail.co.uk>
2019-08-17 21:18:15 +0100

    Attempting to decrypt multiple config files

--- Sources/GitEncryptionTest/main.swift
+++ Sources/GitEncryptionTest/main.swift
@@ -3,7 +3,8 @@ import Foundation
 import SwiftToolbox
 
 do {
-    let config: EmailConfig = try ConfigHandler<EmailConfig>(configFile: "./../Config.json", relativeFrom: #file).load()
+    let _ = try ConfigHandler<EmailConfig>(configFile: "./../OtherConfig.json", relativeFrom: #file).load()
+    let _ = try ConfigHandler<EmailConfig>(configFile: "./../Config.json", relativeFrom: #file).load()
     print("Success")
 } catch {
     print("This did not work")
